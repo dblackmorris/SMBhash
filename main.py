6240857c3445fe7e4344d6ea7682c3930a2bd5ac
Ritik Dubey <blackdeath8765@gmail.com>
2018-10-25 12:59:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from termcolor import colored
 ############################################################################################################################################################################################################################################
 #This Script is based on SMB relay attack which was shown on BLACKHAT 2015 USA(Sharing-More-Than-Just-Your-Files.pdf)                                                                                                                      #
-#Source Video URL-https://www.youtube.com/watch?v=a1dgOO9bALA                                                                                                                                                                              #
+#Source Videos URL-https://www.youtube.com/watch?v=a1dgOO9bALA                                                                                                                                                                              #
 #Source Video Presentation - https://www.blackhat.com/docs/us-15/materials/us-15-Brossard-SMBv2-Sharing-More-Than-Just-Your-Files.pdf                                                                                                      #
 #This Script was working on SMB(server message Block) which is used for File sharing over LAN. So an SMB server can share the image over HTTP and remote user can authernticated with challenge response schenerio with the help of hashes.#
 ############################################################################################################################################################################################################################################
